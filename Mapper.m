53d79195ca434242d6b605aff98b7a77213a2392
Bastian Bechtold <bastian.bechtold@jade-hs.de>
2017-03-16 14:04:59 +0100

    adds progress indicator

--- Mapper.m
+++ Mapper.m
@@ -7,6 +7,7 @@ classdef Mapper < handle
         placeEdit
         styleLabel
         stylePopup
+        activityLabel
     end
 
     properties
@@ -19,13 +20,9 @@ classdef Mapper < handle
             obj.mapax = axes(obj.fig);
             obj.mapax.Position = [0.05, 0.15, 0.9, 0.7];
             obj.map = Map(obj.mapax);
-            if nargin == 1
-                obj.place = place;
+            if nargin == 0
+                place = [];
             end
-            panHandle = pan(obj.fig);
-            panHandle.ActionPostCallback = @obj.panZoomCallback;
-            zoomHandle = zoom(obj.fig);
-            zoomHandle.ActionPostCallback = @obj.panZoomCallback;
 
             obj.placeLabel = uicontrol();
             obj.placeLabel.Style = 'Text';
@@ -39,7 +36,7 @@ classdef Mapper < handle
             obj.placeEdit.Position = [0.22, 0.89, 0.2, 0.05];
             obj.placeEdit.HorizontalAlignment = 'left';
             obj.placeEdit.Callback = @obj.placeEditCallback;
-            obj.placeEdit.String = obj.place;
+            obj.placeEdit.String = place;
 
             obj.styleLabel = uicontrol();
             obj.styleLabel.Style = 'Text';
@@ -53,18 +50,27 @@ classdef Mapper < handle
             obj.stylePopup.Position = [0.62, 0.89, 0.2, 0.05];
             obj.stylePopup.String = obj.map.styles;
             obj.stylePopup.Callback = @obj.styleSelectCallback;
+
+            obj.activityLabel = uicontrol();
+            obj.activityLabel.Style = 'Text';
+            obj.activityLabel.String = '';
+            obj.activityLabel.Units = 'normalized';
+            obj.activityLabel.HorizontalAlignment = 'center';
+            obj.activityLabel.Position = [0.4, 0.02, 0.2, 0.05];
+
+            if nargin == 1
+                obj.place = place;
+            end
+            panHandle = pan(obj.fig);
+            panHandle.ActionPostCallback = @obj.panZoomCallback;
+            zoomHandle = zoom(obj.fig);
+            zoomHandle.ActionPostCallback = @obj.panZoomCallback;
         end
 
         function delete(obj)
             if ishandle(obj.fig)
                 close(obj.fig);
             end
-        end
-
-        function set.place(obj, place)
-            coords = obj.downloadCoords(place);
-            obj.map.coords = coords;
-            obj.place = place;
         end
 
         function coords = downloadCoords(obj, place)
@@ -78,23 +84,38 @@ classdef Mapper < handle
                             'maxLat', geometry.northeast.lat);
         end
 
+        function set.place(obj, place)
+            obj.activityLabel.String = 'Downloading...';
+            coords = obj.downloadCoords(place);
+            obj.map.coords = coords;
+            obj.place = place;
+            obj.activityLabel.String = '';
+        end
+
+
         function placeEditCallback(obj, target, event)
+            obj.activityLabel.String = 'Downloading...';
             obj.place = target.String;
+            obj.activityLabel.String = '';
         end
 
         function styleSelectCallback(obj, target, event)
+            obj.activityLabel.String = 'Downloading...';
             obj.map.style = target.String{target.Value};
+            obj.activityLabel.String = '';
         end
 
         function panZoomCallback(obj, target, event)
             if event.Axes ~= obj.mapax
                 return
             end
+            obj.activityLabel.String = 'Downloading...';
             coords = struct('minLon', obj.mapax.XLim(1), ...
                             'maxLon', obj.mapax.XLim(2), ...
                             'minLat', obj.mapax.YLim(1), ...
                             'maxLat', obj.mapax.YLim(2));
             obj.map.coords = coords;
+            obj.activityLabel.String = '';
         end
     end
 end
